074aaf9ac1736dfabb52b02e72f715b7bec9511c
marferbl <fernandezblascomarcos@gmail.com>
2021-12-22 11:00:10 +0100

    a

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var iconMenu = document.querySelector(".burger");
 console.log(iconMenu);
+var togglesElements = document.querySelectorAll(".toggleElement");
+// var toggleEl2 = document.querySelector(".toggleElement2");
+// var toggleEl3 = document.querySelector(".toggleElement3");
 
 iconMenu.addEventListener("click", () => {
   document.querySelector(".toggle").style.display == "flex"
     ? (document.querySelector(".toggle").style.display = "none")
     : (document.querySelector(".toggle").style.display = "flex");
+});
+
+togglesElements.forEach((elem) => {
+  elem.addEventListener("click", () => setTimeout(() => (document.querySelector(".toggle").style.display = "none")), 1000);
 });
